a6af86fc0710c0a3e4e4807b9a7064758f51016d
Actinium-System <56242751+Actinium-System@users.noreply.github.com>
2019-10-13 16:14:05 +0800

    Update RandomCommon.bat

--- RandomCommon.bat
+++ RandomCommon.bat
@@ -1,3 +1,7 @@
+'This is a common random program.
+'made by AC
+'If you cannot use it please delete the first 3 lines of the code.
+
 @echo off
 title Random 1.0
 set /p a=Please input the maximum integer:
