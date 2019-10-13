131fde337da8842b9e0d9710e8464fd0e1020b4a
Actinium-System <56242751+Actinium-System@users.noreply.github.com>
2019-10-13 15:06:37 +0800

    Create RandomCommon.bat

---
+++ RandomCommon.bat
@@ -0,0 +1,15 @@
+@echo off
+title Random 1.0
+set /p a=Please input the maximum integer:
+echo.
+echo Calculate...
+pause>nul
+:begin
+cls
+set num=%random%
+set /a num=num%%%a%+1
+echo Result:
+echo.
+echo %num%
+pause>nul
+goto begin
